d7a80b1b35237f09a2f174ef53b305144e4bf3f6
Da1sypetals <da1sypetals.iota@gmail.com>
2025-12-05 10:14:51 +0800

    Provide readable message when shape is incompatible in cuda.tile.cat
    Signed-off-by: Da1sypetals <da1sypetals.iota@gmail.com>

--- src/cuda/tile/_ir/ops.py
+++ src/cuda/tile/_ir/ops.py
@@ -2969,8 +2969,8 @@ def cat(tiles: Var, axis: int) -> Var:
             raise TileTypeError(f"Expected tiles to have the same dtype: {dtype} != {tile_ty.dtype}")  # noqa: E501
         for i, (x, y) in enumerate(zip(shape_value, tile_ty.shape_value, strict=True)):
             if i != axis and x != y:
-                raise TileTypeError("Expected tiles to have the same "
-                                    "shape for non axis dimensions")
+                raise TileTypeError("Expected tiles to have the same shape for non axis dimensions, "
+                                    f"got tiles with shapes: {', '.join(str(ty._unwrapped_shape) for ty in tuple_ty.value_types)}")
         shape_value[axis] += tile_ty.shape_value[axis]
 
     if not all(_is_power_of_2(x) for x in shape_value):
